1c71a81bb27611b6a3e90be623169b031b6e0697
Shikki <mary.sh95@gmail.com>
2020-08-21 17:55:42 +0300

    master

---
+++ count_text.py
@@ -0,0 +1,50 @@
+def changeText(text):
+    for i in '!"\'#$%&()*+-,/:;<=>?@[\\]^_{|}~':
+        text = text.replace(i, '')
+        
+    return text.split()
+def mostCommon(text, lenght = 0):
+    most_common = []
+    qty_most_common = 0
+    for item in text:
+        if len(item) > lenght:
+            qty = text.count(item)
+        if qty < 2: 
+            break
+        if qty > qty_most_common:
+            qty_most_common = qty
+            most_common = [item]
+        elif qty == qty_most_common:
+            most_common.append(item)
+            
+    return list(set(most_common))
+
+def mostLenght(text):
+    most_lenght = []
+    qty_most_lenght = 0
+    alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    for item in text:
+        for char in item:
+            if char not in alphabet:
+                charEn = False
+            else:
+                charEn = True
+            
+        if charEn:
+            qty = len(item)
+            if qty > qty_most_lenght:
+                qty_most_lenght = qty
+                most_lenght = [item]
+            elif qty == qty_most_lenght:
+                most_lenght.append(item)
+                
+    return list(set(most_lenght))
+
+nameFile = input('Название файла: ')
+
+with open(nameFile, encoding='utf8') as f:
+    fileText = f.read()
+    
+fileText = changeText(fileText)
+print(f'Список самых частых слов длинной более 3х символов: {mostCommon(fileText, 3)}')
+print(f'Список самых длинных английскиих слов: {mostLenght(fileText)}')
