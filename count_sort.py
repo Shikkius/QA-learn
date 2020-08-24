fde63b263a611859e93b1c37fa01aa1b57390561
Shikki <mary.sh95@gmail.com>
2020-08-25 02:17:16 +0300

    count_sort

--- count_sort.py
+++ count_sort.py
@@ -3,39 +3,40 @@
 
 #заводим псевдорандомный список
 from random import randint
+
+CBL = 0
+def bubble_sort(A):
+    global CBL
+    for i in range(N-1):
+        for j in range(N-i-1):
+            if A[j] > A[j+1]:
+                A[j], A[j+1] = A[j+1], A[j]
+                CBL += 1
+    return A
+
 N = 10
 A = []
 for i in range(N):
     A.append(randint(1, 99))
 print(f'Изначальный список: {A}')
 
-i = 0
-countBubble = 0
-while i < N-1:
-    j = 0
-    countBubble += 1
-    while j < N-i-1:
-        if A[j] > A[j+1]:
-            A[j], A[j+1] = A[j+1], A[j]
-        j += 1
-    i += 1
-print(f'Сортировка пузырьком: {A}')
-
-countInsertinon = 0
+CINS = 0
 def insertion(A):
     for i in range(1, len(A)):
         b = A[i]
         j = i - 1
-        global countInsertinon
+        global CINS
         while (j >= 0) and (b < A[j]):
             A[j+1] = A[j]
             j -= 1
         A[j+1] = b
-        countInsertinon += 1
+        CINS += 1
     return A
 
+bubble_sort(A)
 insertion(A)
+print(f'Сортировка пузырьком: {A}')
 print(f'Сортировка вставками: {A}')
-print(f'Количество перестановок при пузырьковой сотрировке: {countBubble}')
-print(f'Количество перестановок при сортировке вставками: {countInsertinon}')
-print(f'Разница в перестановках: {(countBubble) - (countInsertinon)}')
+print(f'Количество перестановок при пузырьковой сотрировке: {CBL}')
+print(f'Количество перестановок при сортировке вставками: {CINS}')
+print(f'Разница в перестановках: {(CBL) - (CINS)}')
