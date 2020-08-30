e5ef65f37300c8a91a4b778a46cd0915eeeabad1
Shikki <mary.sh95@gmail.com>
2020-08-30 22:01:51 +0300

    findname

---
+++ findname_17 2 1.py
@@ -0,0 +1,37 @@
+group_1 = "Антон Ярослав Ксения Мария Павел Егор Тимофей Павел".split()
+group_2 = "Анастасия Анна Павел Егор Павел Арсений Антон Павел".split()
+group_3 = "Татьяна Сергей Михаил Дмитрий Екатерина Полина".split()
+
+def find_name(g):
+    counter = {}
+    name = "Павел"
+    for name in g:
+        counter[name] = counter.get(name, 0) + 1
+    max_count = max(counter.values())
+    most_frequent = [k for k, v in counter.items() if v == max_count]
+    return max_count
+
+print_gr = 0
+count_gr = 0
+for i in range(1):
+    i = find_name(group_1)
+    if i > count_gr:
+        count_gr = i
+        print_gr = group_1
+    else:
+        break
+for i in range(1):
+    i = find_name(group_2)
+    if i > count_gr:
+        count_gr = i
+        print_gr = group_2
+    else:
+        break
+for i in range(1):
+    i = find_name(group_3)
+    if i > count_gr:
+        count_gr = i
+        print_gr = group_3
+    else:
+        break
+print(print_gr)
